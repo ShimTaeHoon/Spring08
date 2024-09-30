e3300d174a978564fda017b14ee1a6a93bcc1468
shim tae hoon <sth2721@gmail.com>
2024-09-30 17:05:21 +0900

    zxc
    zxc

---
+++ src/main/java/com/example/demo/repository/BoardRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import com.example.demo.entity.Board;
+
+public interface BoardRepository extends JpaRepository<Board, Integer>, QuerydslPredicateExecutor<Board>{
+
+}
+
+/*
+ * 리파지토리 만드는방법
+ * 1. JPARepository를 상속받는다
+ * 2. 제네릭 타입에 엔티티와 PK 타입을 쓴다
+ */
---
+++ src/test/java/com/example/demo/repository/MemberRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.entity.Member;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+	@Autowired
+	MemberRepository repository;
+	
+	@Test
+	public void 회원등록() {
+		
+		Member member1 = new Member("user1", "1234", "둘리");
+		repository.save(member1);
+		
+		Member member2 = new Member("user2", "1234", "또치");
+		repository.save(member2);			
+		
+	}
+	
+	@Test
+	public void 회원목록조회() {
+		
+		List<Member> list = repository.findAll();
+		for(Member member : list) {
+			System.out.println(member);
+		}
+		
+	}
+	
+	@Test
+	public void 회원목록단건조회() {
+		
+		Optional<Member> list = repository.findById("user1");
+		if(list.isPresent()) {
+			Member member = list.get();
+			System.out.println(member);
+		}
+		
+	}
+	
+	@Test
+	public void 회원수정() {
+		Optional<Member> list = repository.findById("user1");
+		
+		if(list.isPresent()) {
+			Member member = list.get();
+			// 일부내용변경
+			member.setName("usermodify");
+			// 데이터 업데이트
+			repository.save(member);
+		}
+		
+	}
+	
+	@Test
+	public void 회원삭제() {
+		repository.deleteById("user2");
+	}
+	
+}
